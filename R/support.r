a9d348cfffaf76d44a18af09e6bff18d13c7bba1
bautheac <o.bautheac@gmail.com>
2018-11-18 15:05:02 +0000

    debug vignette

--- R/support.r
+++ R/support.r
@@ -202,7 +202,8 @@ storethat_update_futures_CFTC <- function(names, ...){
   if (list(...)[["verbose"]]) message("Retrieving futures position data from Bloomberg.\n")
 
   CFTC <- BBG_futures_CFTC(active_contract_tickers = dplyr::filter(data, is.na(ticker))$ticker,
-                           start, as.character(Sys.Date()), verbose = list(...)[["verbose"]])
+                           start = min(data$end), end = as.character(Sys.Date()),
+                           verbose = list(...)[["verbose"]])
 
   if (list(...)[["verbose"]]) message("\nUpdating database with retrieved data.\n")
 
@@ -1171,9 +1172,208 @@ setMethod("db_store", signature = c(object = "FundInfo"), function(object, file)
 
 
 
+# storethat_update
 
+## all_fields_exist ####
+#' Check all fields provided exist in 'support_fields' table.
+#'
+#'
+#' @param fields A data.frame or childs. Existing fields in the object.
+#'
+#' @param con A connection object to the relevant database.
+all_fields_exist <- function(fields, con){
 
+  fields <- dplyr::distinct(dplyr::select(fields, instrument:symbol))
 
+  query <- "SELECT * FROM support_fields;"; query <- RSQLite::dbGetQuery(con = con, query)
+  query <- dplyr::anti_join(fields, query, by = names(fields))
 
+  nrow(query) == 0L
 
+}
+
+## update_fields ####
+#' Updates 'support_fields' table with provided fields.
+#'
+#'
+#' @param fields A data.frame or childs. Existing fields in the object.
+#'
+#' @param con A connection object to the relevant database.
+update_fields <- function(fields, con){
+
+  fields <- dplyr::distinct(dplyr::select(fields, instrument:symbol))
+
+  query <- "SELECT * FROM support_fields;"; query <- RSQLite::dbGetQuery(con = con, query)
+  query <- dplyr::anti_join(fields, query, by = names(fields))
+
+  RSQLite::dbWriteTable(con, "support_fields", query, row.names = FALSE, overwrite = FALSE, append = TRUE)
+
+}
+
+## all_tickers_exist ####
+#' Check all tickers provided exist in the support tickers table specified.
+#'
+#'
+#' @param tickers A character vector. Existing tickers in the object.
+#'
+#' @param table_tickers A scalar character vector. Specifies the table name
+#'   where comparison tickers sit.
+#'
+#' @param con A connection object to the relevant database.
+all_tickers_exist <- function(tickers, table_tickers, con){
+
+  query <- paste0("SELECT DISTINCT ticker FROM ", table_tickers, ";")
+  query <- purrr::flatten_chr(RSQLite::dbGetQuery(con = con, query))
+
+  all(tickers %in% query)
+
+}
+
+## update_tickers ####
+#' Updates the specified support tickers table with provided tickers.
+#'
+#'
+#' @param tickers A character vector. Existing tickers in the object.
+#'
+#' @param table_tickers A scalar character vector. Specifies the table name
+#'   where comparison tickers sit.
+#'
+#' @param con A connection object to the relevant database.
+update_tickers <- function(tickers, table_tickers, con){
+
+  query <- paste0("SELECT DISTINCT ticker FROM ", table_tickers, ";")
+  query <- purrr::flatten_chr(RSQLite::dbGetQuery(con = con, query))
+
+  RSQLite::dbWriteTable(con = con, table_tickers,
+                        tibble::tibble(ticker = tickers[!tickers %in% query]),
+                        row.names = FALSE, overwrite = FALSE, append = TRUE)
+
+}
+
+## update_term_structure_tickers ####
+#' Updates the 'tickers_support_futures_ts' table with provided term structure tickers.
+#'
+#'
+#' @param tickers A character vector. Existing tickers in the object.
+#'
+#' @param con A connection object to the relevant database.
+update_term_structure_tickers <- function(tickers, con){
+
+  tickers <- dplyr::anti_join(tickers,
+                              RSQLite::dbReadTable(con, "tickers_support_futures_ts"),
+                              by = "ticker")
+
+  query <- paste0("SELECT DISTINCT id, ticker FROM tickers_futures WHERE ticker IN ('",
+                  paste(unique(tickers$`active contract ticker`), collapse = "', '"), "');")
+  tickers <- dplyr::left_join(dplyr::filter(tickers, !is.na(ticker)),
+                              RSQLite::dbGetQuery(con = con, query),
+                              by = c("active contract ticker" = "ticker")) %>%
+    dplyr::select(active_contract_ticker_id = id, ticker, position = `TS position`,
+                  roll_type_symbol = `roll type symbol`, roll_days = `roll days`,
+                  roll_months = `roll months`, roll_adjustment_symbol = `roll adjustment symbol`)
+
+  RSQLite::dbWriteTable(con = con, "tickers_support_futures_ts", tickers, row.names = FALSE,
+                        overwrite = FALSE, append = TRUE)
+
+}
+
+## update_cftc_tickers ####
+#' Updates the 'update_cftc_tickers' table with provided CFTC tickers.
+#'
+#'
+#' @param tickers A character vector. Existing tickers in the object.
+#'
+#' @param con A connection object to the relevant database.
+update_cftc_tickers <- function(tickers, con){
+
+  tickers <- dplyr::anti_join(tickers,
+                              RSQLite::dbReadTable(con, "tickers_support_futures_cftc"),
+                              by = "ticker")
+
+  query <- paste0("SELECT DISTINCT id, ticker FROM tickers_futures WHERE ticker IN ('",
+                  paste(unique(tickers$`active contract ticker`), collapse = "', '"), "');")
+  tickers <- dplyr::left_join(dplyr::filter(tickers, !is.na(ticker)),
+                              RSQLite::dbGetQuery(con = con, query),
+                              by = c("active contract ticker" = "ticker")) %>%
+    dplyr::select(active_contract_ticker_id = id, ticker)
+
+  RSQLite::dbWriteTable(con = con, "tickers_support_futures_cftc", tickers, row.names = FALSE,
+                        overwrite = FALSE, append = TRUE)
+
+}
+
+## update_data ####
+#' Updates the data table specified with the historical data dataset provided.
+#'
+#'
+#' @param data A data.frame or childs. Contains candidate data for \code{table_data}
+#'   update.
+#'
+#' @param table_data A scalar character vector. Specifies the name of the table
+#'   to udate with \code{data}.
+#'
+#' @param tickers A data.frame or childs. Tickers corresponding to \code{data}.
+#'
+#' @param fields A data.frame or childs. Fields corresponding to \code{data}.
+#'
+#' @param dates A data.frame or childs. Dates corresponding to \code{data}.
+#'
+#' @param con A connection object to the relevant database.
+update_data <- function(data, table_data, tickers, fields, dates, con){
+
+  data <- dplyr::left_join(data,
+                           tickers,
+                           by = "ticker") %>% dplyr::select(ticker_id = id, field, date, value) %>%
+    dplyr::mutate(field = as.character(field)) %>%
+    dplyr::left_join(fields, by = c("field" = "symbol")) %>%
+    dplyr::select(ticker_id, field_id = id, date, value) %>%
+    dplyr::left_join(dplyr::mutate(dates, date = as.Date(date)), by = "date") %>%
+    dplyr::select(ticker_id, field_id, date_id  = id, value)
+
+  query <- paste0("SELECT ticker_id, field_id, date_id FROM ", table_data, " WHERE ticker_id IN (",
+                  paste(unique(data$ticker_id), collapse = ", "), ") AND field_id IN (",
+                  paste(unique(data$field_id), collapse = ", "), ") AND date_id IN (",
+                  paste(unique(data$date_id), collapse = ", "), ");")
+  query <- RSQLite::dbGetQuery(con = con, query)
+
+  data <- dplyr::anti_join(data, query, by = c("ticker_id", "field_id", "date_id"))
+
+  if (nrow(data) > 0L) RSQLite::dbWriteTable(con = con, table_data, data, row.names = FALSE,
+                                             overwrite = FALSE, append = TRUE)
+
+}
+
+## update_data_cftc ####
+#' Updates the data table specified with the historical data dataset provided.
+#'
+#'
+#' @param data a data.frame or childs. Contains candidate data for \code{table_data}
+#'   update.
+#'
+#' @param table_data a scalar character vector. Specifies the name of the table
+#'   to udate with \code{data}.
+#'
+#' @param tickers a data.frame or childs. Tickers corresponding to \code{data}.
+#'
+#' @param dates a data.frame or childs. Dates corresponding to \code{data}.
+#'
+#' @param con a connection object to the relevant database.
+update_data_cftc <- function(data, table_data, tickers, dates, con){
+
+  data <- dplyr::left_join(data, tickers, by = "ticker") %>%
+    dplyr::select(ticker_id = id, date, value) %>%
+    dplyr::left_join(dplyr::mutate(dates, date = as.Date(date)), by = "date") %>%
+    dplyr::select(ticker_id, date_id = id, value)
+
+  query <- paste0("SELECT ticker_id, date_id FROM ", table_data," WHERE ticker_id IN (",
+                  paste(unique(data$ticker_id), collapse = ", "), ") AND date_id IN (",
+                  paste(unique(data$date_id), collapse = ", "), ");")
+  query <- RSQLite::dbGetQuery(con = con, query)
+
+  data <- dplyr::anti_join(data, query, by = c("ticker_id", "date_id"))
+
+  if (nrow(data) > 0L) RSQLite::dbWriteTable(con = con, table_data, data, row.names = FALSE,
+                                             overwrite = FALSE, append = TRUE)
+
+}
 
--- doc/pullit.R
+++ doc/pullit.R
@@ -1,99 +1,119 @@
 ## ----setup, include = FALSE----------------------------------------------
-knitr::opts_chunk$set(collapse = TRUE, eval = FALSE, comment = "#>")
+knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 
-## ----`equity market`-----------------------------------------------------
-#  library(pullit); library(lubridate)
-#  
-#  end <- Sys.Date() - years(1L); start <- end - years(2L)
+## ----`globals`, message = FALSE------------------------------------------
+library(pullit); library(lubridate)
+
+end <- Sys.Date() - years(1L); start <- end - years(2L)
+
+## ----`equity market BBG`, eval = FALSE-----------------------------------
 #  equity_tickers <- c("ADM US Equity", "KHC US Equity", "XPO US Equity")
-#  
 #  equity_market <- BBG_equity_market(equity_tickers, start, end, verbose = FALSE)
 
-## ----`equity balance sheet`----------------------------------------------
+## ----`equity market storethat`, echo = FALSE-----------------------------
+library(storethat)
+
+equity_tickers <- c("ADM US Equity", "KHC US Equity", "XPO US Equity")
+equity_market <- storethat_equity_market(file = "../data-raw/storethat.sqlite", tickers = equity_tickers, start = start, end = end, verbose = FALSE)
+
+## ----`equity balance sheet`, eval = FALSE--------------------------------
 #  equity_BS <- BBG_equity_book(book = "balance sheet", equity_tickers, start, end, verbose = FALSE)
 
-## ----`equity cash flow statement`----------------------------------------
+## ----`equity cash flow statement`, eval = FALSE--------------------------
 #  equity_CF <- BBG_equity_book(book = "cash flow statement", equity_tickers, start, end, verbose = FALSE)
 
-## ----`equity income statement`-------------------------------------------
+## ----`equity income statement`, eval = FALSE-----------------------------
 #  equity_IS <- BBG_equity_book(book = "income statement", equity_tickers, start, end, verbose = FALSE)
 
-## ----`equity key stats`--------------------------------------------------
+## ----`equity key stats`, eval = FALSE------------------------------------
 #  equity_KS <- BBG_equity_book(book = "key stats", equity_tickers, start, end, verbose = FALSE)
 
-## ----`equity ratios`-----------------------------------------------------
+## ----`equity ratios`, eval = FALSE---------------------------------------
 #  equity_R <- BBG_equity_book(book = "ratios", equity_tickers, start, end, verbose = FALSE)
 
-## ----`equity info`-------------------------------------------------------
+## ----`equity info`, eval = FALSE-----------------------------------------
 #  equity_info <- BBG_equity_info(equity_tickers, verbose = FALSE)
 
-## ----`fund market`-------------------------------------------------------
+## ----`fund market BBG`, eval = FALSE-------------------------------------
 #  fund_tickers <- c("SPY US Equity", "GLD US Equity", "EEM US Equity")
 #  
 #  fund_market <- BBG_fund_market(fund_tickers, start, end, verbose = FALSE)
 
-## ----`fund info`---------------------------------------------------------
+## ----`fund market storethat`, echo = FALSE-------------------------------
+fund_tickers <- c("SPY US Equity", "GLD US Equity", "EEM US Equity")
+
+fund_market <- storethat_fund_market(file = "../data-raw/storethat.sqlite", tickers = fund_tickers, start = start, end = end, verbose = FALSE)
+
+## ----`fund info`, eval = FALSE-------------------------------------------
 #  fund_info <- BBG_fund_info(fund_tickers, verbose = FALSE)
 
-## ----`futures term structure`--------------------------------------------
+## ----`futures term structure BBG`, eval = FALSE--------------------------
 #  futures_tickers <- c("C A Comdty", "EDA Comdty", "ESA Index")
 #  
 #  futures_TS <- BBG_futures_market(type = "term structure", active_contract_tickers = futures_tickers,
 #                                   start, end, TS_positions = 1L:5L, roll_type = "A", roll_days = 0L,
 #                                   roll_months = 0L, roll_adjustment = "N", verbose = FALSE)
 
-## ----`futures aggregated`------------------------------------------------
+## ----`futures term structure storethat`, echo = FALSE, warning = FALSE----
+futures_tickers <- c("C A Comdty", "EDA Comdty", "ESA Index")
+
+futures_TS <- storethat_futures_market(file = "../data-raw/storethat.sqlite", type = "term structure", active_contract_tickers = futures_tickers, start, end, TS_positions = 1L:5L, roll_type = "A", roll_days = 0L, roll_months = 0L, roll_adjustment = "N", verbose = FALSE)
+
+## ----`futures aggregated`, eval = FALSE----------------------------------
 #  futures_agg <- BBG_futures_market(type = "aggregate", active_contract_tickers = futures_tickers,
 #                                    start, end, verbose = FALSE)
 
-## ----`futures CFTC`------------------------------------------------------
+## ----`futures CFTC`, eval = FALSE----------------------------------------
 #  futures_CFTC <- BBG_futures_CFTC(active_contract_tickers = futures_tickers, start, end, verbose = FALSE)
 
-## ----`futures info`------------------------------------------------------
+## ----`futures info`, eval = FALSE----------------------------------------
 #  futures_info <- BBG_futures_info(futures_tickers, verbose = FALSE)
 
 ## ----`equity show`-------------------------------------------------------
-#  equity_market
+equity_market
 
 ## ----`equity get_tickers`------------------------------------------------
-#  get_tickers(equity_market)
+get_tickers(equity_market)
 
 ## ----`equity get_fields`-------------------------------------------------
-#  get_fields(equity_market)
+get_fields(equity_market)
 
 ## ----`equity get_data`---------------------------------------------------
-#  get_data(equity_market)
+get_data(equity_market)
 
 ## ----`equity get_call`---------------------------------------------------
-#  get_call(equity_market)
+get_call(equity_market)
 
 ## ----`equity get_periods`------------------------------------------------
-#  get_periods(equity_market)
+get_periods(equity_market)
 
-## ----`storethat store`---------------------------------------------------
+## ----`storethat store`, eval = FALSE-------------------------------------
 #  library(storethat)
 #  
-#  db_create(path = "~/data-raw/")
+#  db_create()
 #  
-#  data <- list(equity_market, equity_BS, equity_CF, equity_IS, equity_KS, equity_R, equity_info,
-#               fund_market, fund_info, futures_TS, futures_agg, futures_CFTC, futures_info)
+#  db_store(object = futures_TS, file = "~/storethat.sqlite", verbose = FALSE)
+#  db_store(object = fund_market, file = "~/storethat.sqlite", verbose = FALSE)
+
+## ----`storethat retrieve`, eval = FALSE----------------------------------
+#  futures_TS <- storethat_futures_market(
+#    type = "term structure", active_contract_tickers = futures_tickers,start, end,
+#    TS_positions = 1L:5L, roll_type = "A", roll_days = 0L,
+#    roll_months = 0L, roll_adjustment = "N", verbose = FALSE)
 #  
-#  do.call(db_store, data)
+#  fund_market <- storethat_fund_market(fund_tickers, start, end, verbose = FALSE)
 
-## ----`storethat retrieve`------------------------------------------------
-#  equity_market <- storethat_equity_market(equity_tickers, start, end, verbose = FALSE)
+## ----`storethat update all`, eval = FALSE--------------------------------
+#  storethat_update(instrument = "equity", verbose = FALSE)
 
-## ----`storethat update all`----------------------------------------------
-#  storethat_update()
-
-## ----`storethat update some`---------------------------------------------
+## ----`storethat update some`, eval = FALSE-------------------------------
 #  storethat_update(instrument = "equity", book = "market")
 
-## ----`plot term structure`-----------------------------------------------
-#  library(plotit)
-#  
-#  plot_term_structure(object = futures_TS, ticker = "C A Comdty")
+## ----`plot term structure`, fig.fullwidth = TRUE-------------------------
+library(plotit)
 
-## ----`plot performance`--------------------------------------------------
-#  plot_performance(object = fund_market, ticker = "GLD US Equity")
+plot_term_structure(object = futures_TS, ticker = "C A Comdty")
+
+## ----`plot performance`, fig.fullwidth = TRUE----------------------------
+plot_performance(object = fund_market, ticker = "GLD US Equity")
 
